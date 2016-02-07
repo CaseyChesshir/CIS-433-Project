b4f3949674ae0cdbbd6c9159ce3646827ce9d8ae
CaseyChesshir <chesshir@uoregon.edu>
2016-02-06 18:03:59 -0800

    removed duplicates plus simplified finder script

--- ipv6finder.py
+++ ipv6finder.py
@@ -6,5 +6,5 @@ with open('nslookups.txt') as f:
 	for line in f:
 		match = re.search(".+has AAAA address.+",line)
 		if(match):
-			out.write(match.group(0).split('\t')[0] + ',' + match.group(0).split(' ')[-1] + "\n")	
-			#out.write(match.group(0).split(' ')[-1] + "\n")
+			out.write(match.group(0).split('\t')[0] + "\n")
+out.close()
